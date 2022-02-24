d632fbf0b0a31368aacf11e636f6bd85c77710bf
Arthurjim <arthurjim013@gmail.com>
2022-02-24 17:50:28 -0600

    Creando compononente para las entradas

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JournalEntry = () => {
+  return (
+    <div className='journal__entry'>
+        <div></div>
+    </div>
+  )
+}
+
+export default JournalEntry
--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const JournalScreen = () => {
   return (
-    <div>JournalScreen</div>
+    <div className='journal__main-content'>
+      <Sidebar/>
+      <main>
+        <h1>Main Content</h1>
+      </main>
+    </div>
   )
 }
 
