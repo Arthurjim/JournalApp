8a1e320715b87a6f7f778c1ae1ed340593735f0a
Arthurjim <arthurjim013@gmail.com>
2022-03-27 16:18:22 -0600

    Se agrega estilo al journal entry

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const JournalEntry = () => {
-  return (
-    <div className='journal__entry'>
-        <div></div>
-    </div>
-  )
-}
+    return (
+        <div className="journal__entry pointer">
+            <div
+                className="journal__entry-pircture"
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage:
+                        "url(https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)",
+                }}
+            ></div>
+            <div className="journal__entry-body">
+                <p className="jorunal__entry-title">Un nuevo día</p>
+                <p className="jorunal__entry-content">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Nam, officiis delectus odio quibusdam ratione facere totam!
+                    Consequatur ex eaque natus molestiae
+                </p>
+            </div>
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    );
+};
 
-export default JournalEntry
+export default JournalEntry;
