1e23fd856a9ecbf30574939fa7a4a1c563782dea
Arthurjim <arthurjim013@gmail.com>
2022-03-27 23:16:40 -0600

    Se implementa configuracion de redux

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import AppRouter from './routes/AppRouter'
-
+import React from "react";
+import AppRouter from "./routes/AppRouter";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 const JournalApp = () => {
-  return (
-    <div>
-      <AppRouter/>
-    </div>
-  )
-}
+    return (
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
+    );
+};
 
-export default JournalApp
+export default JournalApp;
