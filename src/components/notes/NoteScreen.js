75748d9fa4e2ac11fcd9cfb3ac7ff976e90b4b16
Arthurjim <arthurjim013@gmail.com>
2022-03-27 21:43:52 -0600

    Se termina estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -2,9 +2,29 @@ import React from "react";
 import NotesAppBar from "./NotesAppBar";
 
 const NoteScreen = () => {
-    return <div className="notes__main-content">
-        <NotesAppBar />
-    </div>;
+    return (
+        <div className="notes__main-content">
+            <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happend today"
+                    className="note__textarea"
+                ></textarea>
+                <div className="note__image">
+                    <img
+                        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                        alt="image"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default NoteScreen;
