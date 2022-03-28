da587738907492b769c5a90408777a08d40f7378
Arthurjim <arthurjim013@gmail.com>
2022-03-27 22:59:19 -0600

    Se instala redux y react-redux

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -19,7 +19,7 @@ const NoteScreen = () => {
                 <div className="note__image">
                     <img
                         src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                        alt="image"
+                        alt="images"
                     />
                 </div>
             </div>
