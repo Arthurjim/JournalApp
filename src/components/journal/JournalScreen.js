0055592db7885afbf8a83301912f52141b562c02
Arthurjim <arthurjim013@gmail.com>
2022-03-27 21:14:31 -0600

    Se agrega estilos en compoente NothingSelected

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import NothingSelected from './NothingSelected'
 import Sidebar from './Sidebar'
-
 const JournalScreen = () => {
   return (
     <div className='journal__main-content'>
       <Sidebar/>
       <main>
-        <h1>Main Content</h1>
+        <NothingSelected/>
       </main>
     </div>
   )
