4c9d31430dfebfc07b2b916834bd81a5cb0d42cb
Arthurjim <arthurjim013@gmail.com>
2022-03-30 08:46:49 -0600

    Se muestrn las notas desde el sidebar y se instala moment

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,27 +1,26 @@
 import React from "react";
+import moment from "moment";
+const JournalEntry = ({ id, title, body, date, url }) => {
+    const notesDate = moment(date);
 
-const JournalEntry = () => {
     return (
         <div className="journal__entry pointer">
-            <div
-                className="journal__entry-pircture"
-                style={{
-                    backgroundSize: "cover",
-                    backgroundImage:
-                        "url(https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)",
-                }}
-            ></div>
+            {url && (
+                <div
+                    className="journal__entry-pircture"
+                    style={{
+                        backgroundSize: "cover",
+                        backgroundImage: `url(${url})`,
+                    }}
+                ></div>
+            )}
             <div className="journal__entry-body">
-                <p className="jorunal__entry-title">Un nuevo día</p>
-                <p className="jorunal__entry-content">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam, officiis delectus odio quibusdam ratione facere totam!
-                    Consequatur ex eaque natus molestiae
-                </p>
+                <p className="jorunal__entry-title">{title}</p>
+                <p className="jorunal__entry-content">{body}</p>
             </div>
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{notesDate.format("dddd")}</span>
+                <h4>{notesDate.format("Do")}</h4>
             </div>
         </div>
     );
