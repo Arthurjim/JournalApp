3eef60d7d5e6c7a01b317fb5298e770811c7dd04
Arthurjim <arthurjim013@gmail.com>
2022-03-29 18:02:25 -0600

    Se implementa validacion para Loading en Login

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,22 +1,34 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import {getAuth,onAuthStateChanged} from '@firebase/auth'
-import {useDispatch} from 'react-redux'
+import React, { useEffect, useState } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { useDispatch } from "react-redux";
 import JournalScreen from "../components/journal/JournalScreen";
 import AuthRouter from "./AuthRouter";
 import { login } from "../actions/auth";
 const AppRouter = () => {
     const dispatch = useDispatch();
-
-    useEffect(()=>{
-        const auth =getAuth()
-        onAuthStateChanged(auth,({displayName,uid})=>{
-            if(displayName){
-                dispatch(login(uid,displayName))
+    const [cheking, setCheking] = useState(true);
+    const [isLoggeIn, setIsLoggeIn] = useState(false);
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, ({ displayName, uid }) => {
+            if (displayName) {
+                dispatch(login(uid, displayName));
+                setIsLoggeIn(true);
+            } else {
+                setIsLoggeIn(false);
             }
-        
-        })
-    },[])
+            setCheking(false);
+        });
+    }, [setCheking, setIsLoggeIn]);
+    if (cheking) {
+        return <div>Loading...</div>;
+    }
     return (
         <Router>
             <div>
